85fd00ed4660041480e6ee6ce93159e4d3abf693
henripal <henri.palacci@gmail.com>
2017-05-19 15:17:00 -0400

    refactored code, added tests, added NormalNormal, added dataframe utility for dirichlet

--- src/EmpiricalBayes.jl
+++ src/EmpiricalBayes.jl
@@ -1,7 +1,7 @@
 
 module EmpiricalBayes
 
-export fit_bayes, BayesModel
+export DirichletCategorical, NormalNormal, dirichlet_df
 
 using DataFrames
 using Distributions
@@ -15,13 +15,56 @@ probabilities.
 Step 2: fit a Dirichlet to the distribution of probabilities
 Step 3: per id, output the posterior mean of the dirichlet-categorical.
 """
-type BayesModel
-    n_cat::Int64
-    ids::Array{Any, 1}
+abstract BayesModel{S}
+
+type DirichletCategorical{S} <: BayesModel
+    sample_sizes::Dict{S, Int64}
     prior::Distributions.Dirichlet{Float64}
-    likelihoods::Array{Distributions.Categorical{Float64}, 1}
-    probabilities::Array{Float64, 2}
+    likelihoods::Dict{S, Distributions.Categorical{Float64}}
+    posteriors::Dict{S, Distributions.Dirichlet{Float64}}
 end
+
+type NormalNormal{S} <: BayesModel
+    sample_sizes::Dict{S, Int64}
+    prior::Distributions.Normal{Float64}
+    likelihoods::Dict{S, Distributions.Normal{Float64}}
+    posteriors::Dict{S, Distributions.Normal{Float64}}
+end
+
+function NormalNormal{S}(values::Dict{S, Array{Float64, 1}})
+    sample_sizes = Dict{S, Int64}()
+    likelihoods = Dict{S, Distributions.Normal{Float64}}()
+    posteriors = Dict{S, Distributions.Normal{Float64}}()
+    means = zeros(length(values))
+
+    # this is awful. 
+    all_values = Float64[]
+    for (_, value) in values
+       all_values = vcat(all_values, value) 
+    end
+
+    σ = sqrt(var(all_values)) #n-1?
+
+    for (i, (name, value)) in enumerate(values)
+        sample_sizes[name] = length(value)
+        likelihoods[name] = fit(Normal, value)
+        means[i] = likelihoods[name].μ
+    end
+
+    prior = fit(Normal, means)
+
+    for (name, value) in values
+        # conjugate magic
+        n = sample_sizes[name]
+        posterior_variance = 1/(1/prior.σ^2 + n/σ^2)
+        posterior_mean = (prior.μ/prior.σ^2 + sum(value)/σ^2)*posterior_variance
+        posteriors[name] = Normal(posterior_mean, sqrt(posterior_variance))
+    end
+
+    NormalNormal(sample_sizes, prior, likelihoods, posteriors)
+end
+
+    
 """
 Replace the zeros with epsilon values in the probability vector so the dirichlet
 can be fit?
@@ -35,51 +78,57 @@ function zero_out(p::Array{Float64, 1})
 end
 
 """
-Fit the model to the dataframe
+Fit the dirichlet categorical with an empirical prior
+Return the fitted Dirichlet Categorical object
+counts is a dictionary indexed by any type of key whose values are
+arrays of counts [1, 2, 2, 1, ...]
+n_categories is the number of distinct categories
 """
-function fit_bayes(df::DataFrames.DataFrame, id::Symbol, category::Symbol )
+function DirichletCategorical{S}(counts::Dict{S, Array{Int64, 1}}, n_categories::Int64)
+    sample_sizes = Dict{S, Int64}()
+    likelihoods = Dict{S, Distributions.Categorical{Float64}}()
+    posteriors = Dict{S, Distributions.Dirichlet{Float64}}()
+    P = zeros(length(counts), n_categories)
+
+    for (i, (name, count)) in enumerate(counts)
+        sample_sizes[name] = length(count)
+        likelihoods[name] = fit(Categorical, n_categories, count) 
+        P[i, :] = zero_out(likelihoods[name].p)
+    end
+
+    prior = fit(Dirichlet, P')
+
+    for (name, count) in counts
+        # conjugate magic
+        posteriors[name] = Dirichlet(likelihoods[name].p * sample_sizes[name] + prior.alpha)
+    end
+
+    DirichletCategorical(sample_sizes, prior, likelihoods, posteriors)
+end
+
+        
+"""
+Fit the model to the dataframe.
+This needs df[df[:id] .== id, :][:category] to be an integer vector between
+1 and n_max
+"""
+function dirichlet_df(df::DataFrames.DataFrame, id::Symbol, category::Symbol )
     unique_ids = unique(df[id])
     unique_categories = unique(df[category])
 
     n_ids = length(unique_ids)
     n_cat = length(unique_categories)
 
-    likelihoods = Array{Distributions.Categorical{Float64}, 1}()
-    probabilities = Array{Float64, 2}(n_cat, n_ids)
+    counts = Dict{typeof(unique_ids[1]), Array{Int64, 1}}()
 
-    for i in 1:n_ids
-        categories = df[df[id] .== unique_ids[i], :][category]
-        fitted = fit(Categorical, n_cat, categories)
-        push!(likelihoods, fitted)
-        probabilities[:, i] = zero_out(fitted.p)
+    for name in unique_ids
+        categories = df[df[id] .== name, :][category]
+        counts[name] = categories
     end
 
-    prior = fit_mle(Dirichlet, probabilities)
-
-    return BayesModel(n_cat, unique_ids, prior, likelihoods, probabilities)
+    return DirichletCategorical(counts, n_cat)
 end
 
-function return_posterior(df::DataFrame, id::Symbol, category::Symbol, bm::BayesModel)
-    aggr_df = by(df, [id, category], df->DataFrame(N = size(df, 1)))
-    aggr_df_wide = unstack(aggr_df, id, category, :N)
-    for (colname, colval) in eachcol(aggr_df_wide[:, 2:size(aggr_df_wide)[2]])
-        aggr_df_wide[colname] = convert(Array, colval, 0)
-    end
-
-    aggr_df_wide[:adjusted_average] = 0.0
-
-    # this is where the shrinking happens
-    adjusted_average = Matrix(aggr_df_wide[2:(bm.n_cat + 1)]) .+ bm.prior.alpha'
-    mean = Array{Float64}(size(adjusted_average)[1])
-
-    for i in 1:size(adjusted_average)[1]
-        row = adjusted_average[i, :]
-        row = row/sum(row)
-        aggr_df_wide[i, :adjusted_average] = (row' * collect(1:bm.n_cat))[1]
-    end
-
-    return aggr_df_wide
-end
 
 
 end
